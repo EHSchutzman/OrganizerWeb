48c67e23e7f4e6bfed9f25fc849c747f34553571
Ethan Schutzman <ehschutzman@wpi.edu>
2018-12-07 14:18:19 -0500

    Finished functional requirements.  Verified tests on assignment 1 criterea

--- scripts.js
+++ scripts.js
@@ -34,6 +34,7 @@ function calendar_created() {
     }
 
     var data = JSON.parse(this.responseText);
+    console.log(data)
     writeOutput("Successfully created calendar " + data.calendar_name)
 
 
@@ -57,8 +58,16 @@ function calendar_loaded() {
     }
     console.log(this.responseText)
     var data = JSON.parse(this.responseText);
-    document.getElementById('loaded_calendar').innerHTML = data.name
-    writeOutput("Successfully loaded calendar " + data.name)
+    document.getElementById('loaded_calendar').innerText = 'TEST'
+
+    var str = "Successfully loaded calendar " + data.name
+
+    str += "\n\nStarting Date: " + data.startDate
+    str += "\n\nEnding Date: " + data.endDate
+
+    localStorage.setItem("calendar", data.name)
+    writeOutput(str)
+    getAllDates()
 
 }
 
@@ -126,21 +135,17 @@ function writeOutput(str) {
         str
 }
 
-function getDailySchedule() {
-    var date = document.getElementById('daily_schedule_date').value
-}
-
 
 function scheduleMeeting() {
-    var date = document.getElementById('meeting_date').value
+    var dateTime = document.getElementById('meeting_date').value
     var time = document.getElementById('meeting_time').value
 
-    date = date + " " + time
+    var startTime = dateTime + " " + time
     var attendee = document.getElementById('meeting_attendee').value
 
 
     var location = document.getElementById("meeting_location").value
-    var calendar_name = document.getElementById('loaded_calendar').innerText
+    var calendar_name = localStorage.getItem('calendar')
     if (!calendar_name) {
         alert("Please load a calendar")
         return
@@ -149,17 +154,34 @@ function scheduleMeeting() {
     var obj = {
         "name": name,
         "uid": Date.now().toString(),
-        "start_time": date,
+        "start_time": startTime,
         "location": location,
         "attendee": attendee,
         "calendar_name": calendar_name
     }
 
-    var xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = schedule_meeting;
-    xhr.open("POST", url + "/Alpha/meeting/schedule");
-    xhr.send(JSON.stringify(obj));
 
+    var dates = JSON.parse(localStorage.getItem('dates'))
+
+    var found = false;
+
+    dates.forEach(date =>{
+        if(date.date === dateTime ){
+            found = true
+        }
+    })
+
+    
+    if(found){
+        var xhr = new XMLHttpRequest()
+        xhr.onreadystatechange = schedule_meeting;
+        xhr.open("POST", url + "/Alpha/meeting/schedule");
+        xhr.send(JSON.stringify(obj));
+
+    }else{
+        alert("date not in calendar")
+    }
+   
 }
 
 function schedule_meeting() {
@@ -185,7 +207,7 @@ function cancelMeeting() {
 
     var date = document.getElementById('block_meeting_date').value
     var time = document.getElementById('block_meeting_time').value
-    var calendar_name = document.getElementById('loaded_calendar').innerText
+    var calendar_name = localStorage.getItem('calendar')
     if (!calendar_name) {
         alert("Please load a calendar")
         return
@@ -222,7 +244,7 @@ function cancel_meeting(){
 
 function getMonthlySchedule(){
     var date = document.getElementById('monthly_schedule_month').value
-    var calendar_name = document.getElementById('loaded_calendar').innerText
+    var calendar_name = localStorage.getItem('calendar')
 
     if (!calendar_name) {
         alert("Please load a calendar")
@@ -246,18 +268,31 @@ function monthly_schedule(){
 }
 
 function getDailySchedule(){
-    var date = document.getElementById('daily_schedule_date').value
-    var calendar_name = document.getElementById('loaded_calendar').innerText
+    console.log("Getting dayly ")
+
+    var dateTime = document.getElementById('daily_schedule_date').value
+    var calendar_name = localStorage.getItem('calendar')
 
     if (!calendar_name) {
         alert("Please load a calendar")
         return
     }
 
-    var xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = daily_schedule;
-    xhr.open("GET", url + "/Alpha/schedule?date=" +date  + '&calendar_name=' + calendar_name);
-    xhr.send();
+    var dates = JSON.parse(localStorage.getItem('dates'))
+
+    var found = false;
+
+    dates.forEach(date =>{
+        if(date.date === dateTime ){
+            found = true
+        }
+    })
+    if(found){
+        var xhr = new XMLHttpRequest()
+        xhr.onreadystatechange = daily_schedule;
+        xhr.open("GET", url + "/Alpha/schedule?date=" +dateTime  + '&calendar_name=' + calendar_name);
+        xhr.send();
+    }
 }
 
 function daily_schedule(){
@@ -294,7 +329,7 @@ function sortByKey(array, key) {
 
 function addDayToCalendar(){
     var date = document.getElementById('add_day').value 
-    var calendar_name = document.getElementById('loaded_calendar').innerText
+    var calendar_name = localStorage.getItem('calendar')
 
     if(date === null){
         alert("Please enter a date in the format YYYY-MM-DD")
@@ -327,12 +362,25 @@ function add_day(){
         //handle error
     }
 
-    console.log(JSON.parse(this.responseText))
+    var str = ""
+
+    
+    var resp = JSON.parse(this.responseText)
+
+    if(resp.status === "success"){
+        str = "Successfully Added Day to Calendar"
+    }else{
+        str = "Unable to Add Day to Calendar"
+    }
+    writeOutput(str)
+
+
+    getAllDates()
 }
 
 function removeDayFromCalendar(){
     var date = document.getElementById('remove_day').value 
-    var calendar_name = document.getElementById('loaded_calendar').innerText
+    var calendar_name = localStorage.getItem('calendar')
 
     if(date === null){
         alert("Please enter a date in the format YYYY-MM-DD")
@@ -365,7 +413,18 @@ function remove_day(){
         //handle error
     }
 
-    console.log(JSON.parse(this.responseText))
+    var str = ""
+
+    
+    var resp = JSON.parse(this.responseText)
+
+    if(resp.status === "success"){
+        str = "Successfully Removed Day from Calendar"
+    }else{
+        str = "Unable to Remove Day from Calendar"
+    }
+    writeOutput(str)
+    getAllDates()
 }
 
 
@@ -373,7 +432,7 @@ function remove_day(){
 function closeTimeslot(){
     var date = document.getElementById('close_timeslot_date').value
     var time = document.getElementById('close_timeslot_time').value
-    var name = document.getElementById('loaded_calendar').innerText
+    var name = localStorage.getItem('calendar')
     if(date === null){
         alert("Please enter a date")
         return
@@ -420,8 +479,119 @@ function timeslot_closed(){
 }
 
 function blockAllDaysAtTime(){
+    var time = document.getElementById('block_all_timeslots_time').value
+
+    var calendar_name = localStorage.getItem('calendar')
+
+    if(calendar_name === null){
+        alert("Please load calendar")
+    }
+    if(time === null){
+        alert('Please enter a time in the format HH:MM 24h')
+    }
+    var obj = {
+        "time": time,
+        "calendar_name": calendar_name
+    }
+
+    var xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = blocked_days_at_time;
+    xhr.open("POST", url + "/Alpha/calendar/block-time-slot");
+    xhr.send(JSON.stringify(obj));
+    console.log(obj)
+
+}
+
+function blocked_days_at_time(){
+    if (this.readyState !== 4) return;
+    if (this.status !== 200) {
+        //handle error
+    }
+    var str = "";
+
+    var data = JSON.parse(this.responseText)
+    console.log("RESPONSE")
+    console.log(data)
+    if (data.status === "success") {
+        str = "Successfully Blocked Meetings"
+    }else{
+        str = "Failed to Block Meetings"
+
+    }
+
+    writeOutput(str)
+}
 
 
+function getAllDates(){
+    var calendar_name = localStorage.getItem('calendar')
+    var xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = get_dates;
+    xhr.open("GET", url + "/Alpha/calendar/get-dates?calendar_name=" + calendar_name);
+    xhr.send();
+}
 
-"    /Alpha/calendar/block-time-slot"
+function get_dates(){
+    if (this.readyState !== 4) return;
+    if (this.status !== 200) {
+        //handle error
+    }
+
+    console.log(JSON.parse(this.responseText))
+
+    var dates = JSON.parse(this.responseText)
+    dates = Object.values(dates)
+    
+
+
+    localStorage.setItem('dates', JSON.stringify(dates[0]))
+
+}
+
+function blockWeekday(){
+    var weekday = document.getElementById('block_weekday_weekday').value
+    var time = document.getElementById('block_weekday_time').value
+    var calendar_name = localStorage.getItem('calendar')
+
+    if(calendar_name === null){
+        alert("Please load calendar")
+    }
+    if(time === null){
+        alert('Please enter a time in the format HH:MM 24h')
+    }
+    if(weekday === null){
+        alert("Please enter a weekday")
+    }
+    var obj = {
+        "time": time,
+        "calendar_name": calendar_name,
+        "weekday": weekday
+    }
+
+    var xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = block_weekday;
+    xhr.open("POST", url + "/Alpha/calendar/block-time-slot");
+    xhr.send(JSON.stringify(obj));
+    console.log(obj)
+
+}
+
+function block_weekday(){
+    if (this.readyState !== 4) return;
+    if (this.status !== 200) {
+        //handle error
+    }
+    var str = "";
+
+    var data = JSON.parse(this.responseText)
+    console.log("RESPONSE")
+    console.log(data)
+    if (data.status === "success") {
+        str = "Successfully Blocked Meetings on Weekday"
+    }else{
+        str = "Failed to Block Meetings on Weekday"
+
+    }
+
+    writeOutput(str)
 }
